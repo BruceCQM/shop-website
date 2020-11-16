bb6b017c1e14cf16da2bfd6f02da041a6f0625f1
BruceCQM <74523362+BruceCQM@users.noreply.github.com>
2020-11-16 14:34:36 +0800

    Add files via upload

---
+++ Shop/src/cn/bruce/domain/Goods.java
@@ -0,0 +1,40 @@
+package cn.bruce.domain;
+
+public class Goods {
+    private int id;
+    private String goodsname;
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGoodsname() {
+        return goodsname;
+    }
+
+    public void setGoodsname(String goodsname) {
+        this.goodsname = goodsname;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "id=" + id +
+                ", goodsname='" + goodsname + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
